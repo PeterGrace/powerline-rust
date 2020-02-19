8b663b938441e7c034c4053cd66f6245a548c2ec
Peter Grace <pgrace@10thmagnitude.com>
2020-02-19 07:36:30 -0500

    move kube context around and remove user, I don't really need user

--- src/bin/powerline.rs
+++ src/bin/powerline.rs
@@ -5,10 +5,10 @@ use powerline::{modules::*, theme::SimpleTheme};
 fn main() -> powerline::R<()> {
 	let mut prompt = powerline::Powerline::new();
 
-	prompt.add_module(User::<SimpleTheme>::new())?;
+	prompt.add_module(K8sContext::<SimpleTheme>::new())?;
+//	prompt.add_module(User::<SimpleTheme>::new())?;
 	prompt.add_module(Host::<SimpleTheme>::new())?;
 	prompt.add_module(Cwd::<SimpleTheme>::new(45, 4, false))?;
-	prompt.add_module(K8sContext::<SimpleTheme>::new())?;
 	prompt.add_module(Git::<SimpleTheme>::new())?;
 	prompt.add_module(ReadOnly::<SimpleTheme>::new())?;
     prompt.add_module(NewLine::<SimpleTheme>::new())?;
