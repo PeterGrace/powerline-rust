7f7ed7c1be4c06d227f0bec28d1a0ce47428e278
Peter Grace <pgrace@10thmagnitude.com>
2020-02-19 07:35:15 -0500

    why not add a little helm

--- src/modules/k8scontext.rs
+++ src/modules/k8scontext.rs
@@ -25,10 +25,10 @@ impl<S: K8sContextScheme> Module for K8sContext<S> {
         let context = Exec::shell("kubectl config current-context")
             .stdout(Redirection::Pipe)
             .capture()?.stdout_str();
-        let namespace = Exec::shell(format!("kubectl config view -o=jsonpath='{{.contexts[?(@.name==\"{}\")].context.namespace}}'",context.trim_right()))
+        let namespace = Exec::shell(format!("kubectl config view -o=jsonpath='{{.contexts[?(@.name==\"{}\")].context.namespace}}'",context.trim_end()))
             .stdout(Redirection::Pipe)
             .capture()?.stdout_str();
-		segments.push(Segment::simple(format!(" {}/{} ", context.trim_end(), namespace.trim_end()), S::K8S_FG, S::K8S_BG));
+		segments.push(Segment::simple(format!("\u{2388} {}/{} ", context.trim_end(), namespace.trim_end()), S::K8S_FG, S::K8S_BG));
 		Ok(())
 	}
 }
