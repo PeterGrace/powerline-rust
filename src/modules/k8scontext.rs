3ac578770b0573f4ff843ca9ecc2709913ad5421
Peter Grace <pete.grace@gmail.com>
2023-06-14 12:21:57 +0000

    don't use kubectl, just read the yaml directly

--- src/modules/k8scontext.rs
+++ src/modules/k8scontext.rs
@@ -22,12 +22,19 @@ impl<S: K8sContextScheme> K8sContext<S> {
 
 impl<S: K8sContextScheme> Module for K8sContext<S> {
 	fn append_segments(&mut self, segments: &mut Vec<Segment>) -> R<()> {
-        let context = Exec::shell("kubectl config current-context")
+        let  k8s_csv= Exec::shell("yq eval '.current-context as $current| .contexts[]|select(.name==$current)|[.context.cluster,.context.namespace]|@csv' ~/.kube/config")
+            .stdout(Redirection::Pipe)
+            .capture()?.stdout_str();
+        let (context,namespace) = k8s_csv.split_once(",").unwrap();
+
+
+/*        let context = Exec::shell("kubectl config current-context")
             .stdout(Redirection::Pipe)
             .capture()?.stdout_str();
         let namespace = Exec::shell(format!("kubectl config view -o=jsonpath='{{.contexts[?(@.name==\"{}\")].context.namespace}}'",context.trim_end()))
             .stdout(Redirection::Pipe)
             .capture()?.stdout_str();
+*/
 		segments.push(Segment::simple(format!("\u{2388} {}/{} ", context.trim_end(), namespace.trim_end()), S::K8S_FG, S::K8S_BG));
 		Ok(())
 	}
