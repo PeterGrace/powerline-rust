559a568ccd40c0561ec2cf1905ba934b7ccb00b0
Peter Grace <petegrace@stackblitz.com>
2024-01-23 12:09:26 -0500

    wee

--- src/modules/k8scontext.rs
+++ src/modules/k8scontext.rs
@@ -25,17 +25,9 @@ impl<S: K8sContextScheme> Module for K8sContext<S> {
         let  k8s_csv= Exec::shell("yq eval '.current-context as $current| .contexts[]|select(.name==$current)|[.context.cluster,.context.namespace]|@csv' ~/.kube/config")
             .stdout(Redirection::Pipe)
             .capture()?.stdout_str();
-        let (context,namespace) = k8s_csv.split_once(",").unwrap();
-
-
-/*        let context = Exec::shell("kubectl config current-context")
-            .stdout(Redirection::Pipe)
-            .capture()?.stdout_str();
-        let namespace = Exec::shell(format!("kubectl config view -o=jsonpath='{{.contexts[?(@.name==\"{}\")].context.namespace}}'",context.trim_end()))
-            .stdout(Redirection::Pipe)
-            .capture()?.stdout_str();
-*/
-		segments.push(Segment::simple(format!("\u{2388} {}/{} ", context.trim_end(), namespace.trim_end()), S::K8S_FG, S::K8S_BG));
+        if let Some((context,namespace)) = k8s_csv.split_once(",") {
+		    segments.push(Segment::simple(format!("\u{2388} {}/{} ", context.trim_end(), namespace.trim_end()), S::K8S_FG, S::K8S_BG));
+        }
 		Ok(())
 	}
 }
