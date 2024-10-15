245cb2b42da05e54bdbc6c84f8fa86c2ff658017
Peter Grace <pete.grace@gmail.com>
2024-10-14 20:47:41 -0400

    clean up k8s lookup

--- src/modules/k8scontext.rs
+++ src/modules/k8scontext.rs
@@ -1,4 +1,6 @@
 use std::{marker::PhantomData,};
+use std::fs::File;
+use serde_yaml::Value;
 use subprocess::{Exec, Redirection};
 use super::Module;
 use crate::{terminal::Color, Segment, R};
@@ -22,19 +24,19 @@ impl<S: K8sContextScheme> K8sContext<S> {
 
 impl<S: K8sContextScheme> Module for K8sContext<S> {
 	fn append_segments(&mut self, segments: &mut Vec<Segment>) -> R<()> {
-        let  k8s_csv= Exec::shell("yq eval '.current-context as $current| .contexts[]|select(.name==$current)|[.context.cluster,.context.namespace]|@csv' ~/.kube/config")
-            .stdout(Redirection::Pipe)
-            .capture()?.stdout_str();
-        let (context,namespace) = k8s_csv.split_once(",").unwrap();
-
-
-/*        let context = Exec::shell("kubectl config current-context")
-            .stdout(Redirection::Pipe)
-            .capture()?.stdout_str();
-        let namespace = Exec::shell(format!("kubectl config view -o=jsonpath='{{.contexts[?(@.name==\"{}\")].context.namespace}}'",context.trim_end()))
-            .stdout(Redirection::Pipe)
-            .capture()?.stdout_str();
-*/
+		let mut context: String = String::default();
+		let mut namespace: String = String::default();
+		if let Ok(fd) = File::open("/home/pgrace/.kube/config") {
+			if let Ok(v) = serde_yaml::from_reader::<File, Value>(fd) {
+				context = v["current-context"].as_str().unwrap_or("").to_string();
+				if let Some(contexts) = v.get("contexts") {
+					if let Some(array) = contexts.as_sequence() {
+						let cur_context = array.iter().find(|x| x.get("name").unwrap().as_str() == Some(&context)).unwrap();
+						namespace = cur_context["context"]["namespace"].as_str().unwrap_or("").to_string();
+					}
+				}
+			}
+		}
 		segments.push(Segment::simple(format!("\u{2388} {}/{} ", context.trim_end(), namespace.trim_end()), S::K8S_FG, S::K8S_BG));
 		Ok(())
 	}
